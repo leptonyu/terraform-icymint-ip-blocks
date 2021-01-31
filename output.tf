ed2b935bf7e5458416c1467eae205d81c47d0bc1
Daniel YU <i@icymint.me>
2021-01-31 15:43:28 +0800

    update ip

--- output.tf
+++ output.tf
@@ -1,3 +1,10 @@
 output "blocks" {
   value = local.shink_layered_blocks
 }
+
+resource "local_file" "ip_blocks" {
+  content              = join("\n", local.shink_layered_blocks.default)
+  file_permission      = "0644"
+  directory_permission = "0755"
+  filename             = format("%s/as/all.txt", path.module)
+}
