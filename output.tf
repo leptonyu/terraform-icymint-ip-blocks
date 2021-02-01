8bf5e15d648cf1c4e3f1835dc767ce09932df3f4
nuc <i@icymint.me>
2021-02-01 09:06:24 +0800

    fix no default

--- output.tf
+++ output.tf
@@ -3,8 +3,9 @@ output "blocks" {
 }
 
  resource "local_file" "ip_blocks" {
-   content              = join("\n", local.shink_layered_blocks.default)
+   for_each             = local.shink_layered_blocks
+   content              = join("\n", each.value)
    file_permission      = "0644"
    directory_permission = "0755"
-   filename             = format("%s/as/all.txt", path.module)
+   filename             = format("%s/as/%s.txt", path.module, each.key)
  }
