122ed51ec220fcb7e890e9ae39d80afd8603e711
nuc <i@icymint.me>
2021-02-01 08:26:40 +0800

    update default

--- output.tf
+++ output.tf
@@ -2,9 +2,9 @@ output "blocks" {
   value = local.shink_layered_blocks
 }
 
-# resource "local_file" "ip_blocks" {
-#   content              = join("\n", local.shink_layered_blocks.default)
-#   file_permission      = "0644"
-#   directory_permission = "0755"
-#   filename             = format("%s/as/all.txt", path.module)
-# }
+ resource "local_file" "ip_blocks" {
+   content              = join("\n", local.shink_layered_blocks.default)
+   file_permission      = "0644"
+   directory_permission = "0755"
+   filename             = format("%s/as/all.txt", path.module)
+ }
