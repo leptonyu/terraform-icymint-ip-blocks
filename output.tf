a69a8353bd7994620188a26022306b40232c8b28
Daniel YU <i@icymint.me>
2021-02-01 21:25:27 +0800

    update variable

--- output.tf
+++ output.tf
@@ -2,10 +2,10 @@ output "blocks" {
   value = local.shink_layered_blocks
 }
 
- resource "local_file" "ip_blocks" {
-   for_each             = local.shink_layered_blocks
-   content              = join("\n", each.value)
-   file_permission      = "0644"
-   directory_permission = "0755"
-   filename             = format("%s/as/%s.txt", path.module, each.key)
- }
+resource "local_file" "ip_blocks" {
+  for_each             = local.shink_layered_blocks
+  content              = join("\n", each.value)
+  file_permission      = "0644"
+  directory_permission = "0755"
+  filename             = format("%s/%s.txt", var.as_path, each.key)
+}
